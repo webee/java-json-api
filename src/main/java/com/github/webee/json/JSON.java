bd7e0503060f403220fd85912b47fb6cca61bc93
webee.yw <webee.yw@gmail.com>
2016-11-27 04:14:48 +0800

    update.

--- src/main/java/com/github/webee/json/JSON.java
+++ src/main/java/com/github/webee/json/JSON.java
@@ -1,11 +1,15 @@
 package com.github.webee.json;
 
+import java.util.Map;
+
 /**
  * Created by webee on 16/11/25.
  */
 public interface JSON {
     JSONObject newObject();
+    JSONObject newObject(Map<String, Object> map);
     JSONArray newArray();
+    JSONArray newArray(Object[] array);
     Object parse(String text);
     JSONObject parseObject(String text);
     JSONArray parseArray(String text);
